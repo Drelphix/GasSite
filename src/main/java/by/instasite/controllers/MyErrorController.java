b082a8025ef81b422584dd98d0a402b152489dba
Drelphix <alexeydemeshko@gmail.com>
2020-01-29 11:44:43 +0300

    500,404,503

--- src/main/java/by/instasite/controllers/MyErrorController.java
+++ src/main/java/by/instasite/controllers/MyErrorController.java
@@ -3,6 +3,7 @@ package by.instasite.controllers;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
@@ -10,12 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class MyErrorController implements ErrorController {
 
+    @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
-
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "error-404";
             } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
@@ -24,7 +24,7 @@ public class MyErrorController implements ErrorController {
                 return "error-503";
             }
         }
-        return "error";
+        return "error-500";
     }
 
     @Override

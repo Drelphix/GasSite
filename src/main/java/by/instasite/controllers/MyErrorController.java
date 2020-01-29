7a1c713a92117c331f254164010022331d4a8c6b
Drelphix <alexeydemeshko@gmail.com>
2020-01-29 12:38:07 +0300

    500,404,503

--- src/main/java/by/instasite/controllers/MyErrorController.java
+++ src/main/java/by/instasite/controllers/MyErrorController.java
@@ -18,15 +18,12 @@ public class MyErrorController implements ErrorController {
             Integer statusCode = Integer.valueOf(status.toString());
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "error-404";
-            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error-500";
             } else if (statusCode == HttpStatus.SERVICE_UNAVAILABLE.value()) {
                 return "error-503";
             }
         }
         return "error-500";
     }
-
     @Override
     public String getErrorPath() {
         return "/error";

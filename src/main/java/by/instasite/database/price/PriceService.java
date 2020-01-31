be5a2c70e346a50193defdaa4d09cb6376fe1035
FaustMont <slavyn21@gmail.com>
2020-02-01 01:58:02 +0300

    Add Price

--- src/main/java/by/instasite/database/price/PriceService.java
+++ src/main/java/by/instasite/database/price/PriceService.java
@@ -6,8 +6,6 @@ import java.util.List;
 public interface PriceService {
     Price getPriceById(int id);
 
-    //int getDiscountByTelephone(String telephone);
-
     void addPrice(Price price);
 
     void updatePrice(int id, int idStation, int idFuel, double price);
